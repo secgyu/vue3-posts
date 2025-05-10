8bffede0b676e426757f751f0637afbe5e4147c2
김규민 <abc2232002@gmail.com>
2025-05-10 15:31:59 +0900

    first commit

---
+++ src/views/posts/PostCreateView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>게시글 등록</h2>
+    <hr class="my-4" />
+    <PostForm v-model:title="form.title" v-model:content="form.content" @submit.prevent="save">
+      <template #actions>
+        <div class="pt-4">
+          <button type="button" class="btn btn-outline-dark me-2" @click="goListPage">목록</button>
+          <button class="btn btn-primary">저장</button>
+        </div>
+      </template>
+    </PostForm>
+  </div>
+</template>
+
+<script setup>
+import { createPost } from '@/api/posts'
+import PostForm from '@/components/posts/PostForm.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const form = ref({
+  title: null,
+  content: null,
+})
+const save = async () => {
+  try {
+    await createPost({
+      ...form.value,
+      createdAt: Date.now(),
+    })
+    router.push({ name: 'PostList' })
+  } catch (err) {
+    console.error(err)
+  }
+}
+const goListPage = () => {
+  router.push({ name: 'PostList' })
+  // router.push('/post')
+}
+</script>
+
+<style lang="scss" scoped></style>
